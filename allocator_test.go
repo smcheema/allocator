a55a207b9f150c7765c6b1cad55d6dfdb42e7fe1
Saad Cheema <71226369+smcheema@users.noreply.github.com>
2021-08-07 00:12:27 -0400

    Implement simple allocator, with small testing suite. (#3)
    * Implement simple allocator, with small testing suite.
    
    * Run gofmt.
    
    Co-authored-by: Saad Cheema <saadcheema@Saads-MacBook-Air.local>

--- allocator_test.go
+++ allocator_test.go
@@ -1,6 +1,7 @@
 package allocator
 
 import (
+	"github.com/stretchr/testify/require"
 	"testing"
 
 	"github.com/irfansharif/or-tools/cpsatsolver"
@@ -8,4 +9,20 @@ import (
 
 func TestLibraryImport(t *testing.T) {
 	var _ cpsatsolver.Model
+}
+
+func TestAllocationForThreeRanges(t *testing.T) {
+	ranges := [3]_range{{tag: 0}, {tag: 1}, {tag: 1 << 1}}
+	allocations := allocate(ranges[0:2])
+	// assert that row holds a sum of exactly one,
+	// the allocator returns an a two dimensional array of assignments,
+	// [i][j] --> replica i assigned to node j, hence assert for each row
+	// that there exists one and only one value equal to unity.
+	for replicaCount := 0; replicaCount < len(allocations); replicaCount++ {
+		sum := 0
+		for nodeCount := 0; nodeCount < len(allocations[replicaCount]); nodeCount++ {
+			sum += allocations[replicaCount][nodeCount]
+		}
+		require.Equal(t, 1, sum)
+	}
 }
