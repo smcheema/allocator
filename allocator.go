bd0931e78d5fcedcf9e84a0c962811f8aedd75d3
Saad Cheema <71226369+smcheema@users.noreply.github.com>
2021-08-25 20:31:08 -0400

    [August-Milestone #2] Go formatting, add capacity constraint. (#5)

--- allocator.go
+++ allocator.go
@@ -6,43 +6,93 @@ import (
 )
 
 type _range struct {
-	tag int64
+	rangeId RangeId
+	tags    Tags
+	demands Demands
 }
 
-func allocate(ranges []_range) [][]int {
+type Allocator interface {
+	addAssignLikeReplicasToDifferentNodesConstraint()
+	addAdhereToNodeDiskSpaceConstraint()
+	allocate() (ok bool, assignments Assignments)
+}
+
+type CKAllocator struct {
+	ranges           []_range
+	config           *Configuration
+	model            *cpsatsolver.Model
+	constraintMatrix ConstraintMatrix
+}
+
+func initAllocator(ranges []_range, config *Configuration) Allocator {
 	model := cpsatsolver.NewModel()
-	assignment := initAssignmentMatrix(model, ranges)
-	addAssignToSomeNodeConstraint(model, assignment)
-	return constructResult(model, assignment)
+	constraintMatrix := initAssignmentMatrix(model, ranges, config.getClusterSize())
+	return Allocator(&CKAllocator{
+		ranges:           ranges,
+		config:           config,
+		model:            model,
+		constraintMatrix: constraintMatrix,
+	},
+	)
 }
 
-func initAssignmentMatrix(model *cpsatsolver.Model, ranges []_range) [][]cpsatsolver.Literal {
-	assignment := make([][]cpsatsolver.Literal, len(ranges))
+func (allocator *CKAllocator) addAssignLikeReplicasToDifferentNodesConstraint() {
+	for _rangeIndex := 0; _rangeIndex < len(allocator.constraintMatrix); _rangeIndex++ {
+		allocator.model.AddConstraints(cpsatsolver.NewExactlyKConstraint(allocator.config.getReplicationFactor(), allocator.constraintMatrix[_rangeIndex]...))
+	}
+}
+
+func (allocator *CKAllocator) addAdhereToNodeDiskSpaceConstraint() {
+	rangeSizes := extractRangeSizes(allocator.ranges)
+	for nodeIndex := 0; nodeIndex < len(allocator.constraintMatrix[0]); nodeIndex++ {
+		nodeAssignments := make([]cpsatsolver.IntVar, len(allocator.constraintMatrix))
+		for _rangeIndex := 0; _rangeIndex < len(allocator.constraintMatrix); _rangeIndex++ {
+			nodeAssignments[_rangeIndex] = allocator.constraintMatrix[_rangeIndex][nodeIndex].(cpsatsolver.IntVar)
+		}
+		allocator.model.AddConstraints(cpsatsolver.NewLinearConstraint(
+			cpsatsolver.NewLinearExpr(nodeAssignments, rangeSizes, 0),
+			cpsatsolver.NewDomain(0, int64(allocator.config.nodes[nodeIndex].resources[DiskCapacityResource]))),
+		)
+	}
+}
+
+func extractRangeSizes(ranges []_range) []int64 {
+	sizes := make([]int64, len(ranges))
+	for index := 0; index < len(sizes); index++ {
+		sizes[index] = int64(ranges[index].demands[SizeOnDiskDemand])
+	}
+	return sizes
+}
+
+func (allocator *CKAllocator) allocate() (ok bool, assignments Assignments) {
+	return solveAndConstructResult(allocator.model, allocator.constraintMatrix, allocator.ranges)
+}
+
+func initAssignmentMatrix(model *cpsatsolver.Model, ranges []_range, clusterSize int) ConstraintMatrix {
+	assignment := make(ConstraintMatrix, len(ranges))
 	for _rangeIndex := range assignment {
-		assignment[_rangeIndex] = make([]cpsatsolver.Literal, ClusterSize)
+		assignment[_rangeIndex] = make([]cpsatsolver.Literal, clusterSize)
 		for nodeIndex := 0; nodeIndex < len(assignment[_rangeIndex]); nodeIndex++ {
-			// let each entry in the assignment matrix be a boolean literal, indicating presence of a
-			// replica on a specified node or the inverse.
 			assignment[_rangeIndex][nodeIndex] = model.NewLiteral(fmt.Sprintf("Assignment for range %d to node %d", _rangeIndex, nodeIndex))
 		}
 	}
 	return assignment
 }
 
-func addAssignToSomeNodeConstraint(model *cpsatsolver.Model, assignment [][]cpsatsolver.Literal) {
-	for _rangeIndex := 0; _rangeIndex < len(assignment); _rangeIndex++ {
-		model.AddConstraints(cpsatsolver.NewExactlyKConstraint(ReplicationFactor, assignment[_rangeIndex]...))
-	}
-}
-
-func constructResult(model *cpsatsolver.Model, assignment [][]cpsatsolver.Literal) [][]int {
+func solveAndConstructResult(model *cpsatsolver.Model, assignment ConstraintMatrix, ranges []_range) (bool, Assignments) {
 	result := model.Solve()
-	allocatedAssignments := make([][]int, len(assignment))
-	for index := range allocatedAssignments {
-		allocatedAssignments[index] = make([]int, ClusterSize)
-		for _assignmentIndex := range allocatedAssignments[index] {
-			allocatedAssignments[index][_assignmentIndex] = int(result.Value(assignment[index][_assignmentIndex]))
+	if result.Infeasible() || result.Invalid() {
+		return false, nil
+	} else {
+		allocatedAssignments := make(Assignments)
+		for index := range assignment {
+			for _assignmentIndex := range assignment[index] {
+				tempCompute := result.Value(assignment[index][_assignmentIndex])
+				if tempCompute > 0 {
+					allocatedAssignments[ranges[index].rangeId] = append(allocatedAssignments[ranges[index].rangeId], NodeId(_assignmentIndex))
+				}
+			}
 		}
+		return true, allocatedAssignments
 	}
-	return allocatedAssignments
 }
--- allocator_test.go
+++ allocator_test.go
@@ -1,24 +1,265 @@
 package allocator
 
 import (
-	"github.com/irfansharif/or-tools/cpsatsolver"
 	"github.com/stretchr/testify/require"
+	"math/rand"
 	"testing"
 )
 
-func TestLibraryImport(t *testing.T) {
-	var _ cpsatsolver.Model
+func TestGIVENReplicationConstraintTHENAppropriateReplicasAllocated(t *testing.T) {
+	const RangeSizeLimit = 20
+	const ReplicationFactor = 3
+	const TestClusterSize = ResourceAmount(64)
+	testConfig := initConfiguration(buildCluster(TestClusterSize, nodeCapacitySupplier(TestClusterSize, 0, 1)), ReplicationFactor)
+	rangesToAllocate := make([]_range, RangeSizeLimit)
+	for initIndex := range rangesToAllocate {
+		rangesToAllocate[initIndex] = _range{
+			rangeId: RangeId(initIndex),
+			tags:    nil,
+			demands: nil,
+		}
+	}
+	for rangeIndex := range rangesToAllocate {
+		allocator := initAllocator(rangesToAllocate[:rangeIndex], testConfig)
+		allocator.addAssignLikeReplicasToDifferentNodesConstraint()
+		status, allocation := allocator.allocate()
+		require.True(t, status)
+		for _, nodeAssignments := range allocation {
+			require.Equal(t, len(nodeAssignments), ReplicationFactor)
+			require.True(t, isValidNodeAssignment(nodeAssignments, TestClusterSize))
+			require.True(t, isEachReplicaAssignedToDifferentNode(nodeAssignments, TestClusterSize))
+		}
+	}
 }
 
-func TestAllocationForThreeRanges(t *testing.T) {
-	ranges := [3]_range{{tag: 0}, {tag: 1}, {tag: 1 << 1}}
-	allocations := allocate(ranges[0:2])
-	for replicaCount := 0; replicaCount < len(allocations); replicaCount++ {
-		sum := 0
-		for nodeCount := 0; nodeCount < len(allocations[replicaCount]); nodeCount++ {
-			sum += allocations[replicaCount][nodeCount]
-			require.LessOrEqual(t, allocations[replicaCount][nodeCount], 1)
+func TestGIVENReplicationConstraintWHENInsufficientNodesTHENAllocationFails(t *testing.T) {
+	const RangeSizeLimit = 20
+	const ReplicationFactor = 3
+	const TestClusterSize = ResourceAmount(1)
+	testConfig := initConfiguration(buildCluster(TestClusterSize, nodeCapacitySupplier(TestClusterSize, 0, 1)), ReplicationFactor)
+	rangesToAllocate := make([]_range, RangeSizeLimit)
+	for initIndex := range rangesToAllocate {
+		rangesToAllocate[initIndex] = _range{
+			rangeId: RangeId(initIndex),
+			tags:    nil,
+			demands: nil,
 		}
-		require.Equal(t, sum, ReplicationFactor)
 	}
+	allocator := initAllocator(rangesToAllocate, testConfig)
+	allocator.addAssignLikeReplicasToDifferentNodesConstraint()
+	status, allocation := allocator.allocate()
+	require.False(t, status)
+	require.Nil(t, allocation)
+}
+
+func TestGIVENReplicationConstraintWHENInfeasibleReplicationFactorTHENAllocationFails(t *testing.T) {
+	const RangeSizeLimit = 20
+	const ReplicationFactor = 128
+	const TestClusterSize = ResourceAmount(64)
+	testConfig := initConfiguration(buildCluster(TestClusterSize, nodeCapacitySupplier(TestClusterSize, 0, 1)), ReplicationFactor)
+	rangesToAllocate := make([]_range, RangeSizeLimit)
+	for initIndex := range rangesToAllocate {
+		rangesToAllocate[initIndex] = _range{
+			rangeId: RangeId(initIndex),
+			tags:    nil,
+			demands: nil,
+		}
+	}
+	allocator := initAllocator(rangesToAllocate, testConfig)
+	allocator.addAssignLikeReplicasToDifferentNodesConstraint()
+	status, allocation := allocator.allocate()
+	require.False(t, status)
+	require.Nil(t, allocation)
+}
+
+func TestGIVENCapacityConstraintWHENMultipleAllocationsPossibleTHENAllocationSucceeds(t *testing.T) {
+	const RangeSizeLimit = 20
+	const ReplicationFactor = 1
+	const TestClusterSize = ResourceAmount(8)
+	testConfig := initConfiguration(buildCluster(TestClusterSize, nodeCapacitySupplier(TestClusterSize, 8_000, 10_000)), ReplicationFactor)
+	rangesToAllocate := make([]_range, RangeSizeLimit)
+	for initIndex := range rangesToAllocate {
+		rangesToAllocate[initIndex] = _range{
+			rangeId: RangeId(initIndex),
+			tags:    nil,
+			demands: Demands{SizeOnDiskDemand: ResourceAmount(initIndex)},
+		}
+	}
+	allocator := initAllocator(rangesToAllocate, testConfig)
+	allocator.addAssignLikeReplicasToDifferentNodesConstraint()
+	allocator.addAdhereToNodeDiskSpaceConstraint()
+	status, allocation := allocator.allocate()
+	require.True(t, status)
+	for _, nodeAssignments := range allocation {
+		require.Equal(t, len(nodeAssignments), ReplicationFactor)
+		require.True(t, isValidNodeAssignment(nodeAssignments, TestClusterSize))
+	}
+}
+
+func TestGIVENCapacityConstraintWHENSingleAllocationPossibleTHENAllocationSucceeds(t *testing.T) {
+	const RangeSizeLimit = 10
+	const ReplicationFactor = 1
+	const TestClusterSize = ResourceAmount(3)
+	testConfig := initConfiguration(buildCluster(TestClusterSize, []ResourceAmount{70, 80, 90}), ReplicationFactor)
+	rangesToAllocate := make([]_range, RangeSizeLimit)
+	rangeDiskSpaceDemands := [RangeSizeLimit]ResourceAmount{85, 75, 12, 11, 10, 9, 8, 7, 6, 6}
+	for initIndex := range rangesToAllocate {
+		rangesToAllocate[initIndex] = _range{
+			rangeId: RangeId(initIndex),
+			tags:    nil,
+			demands: Demands{SizeOnDiskDemand: rangeDiskSpaceDemands[initIndex]},
+		}
+	}
+	allocator := initAllocator(rangesToAllocate, testConfig)
+	allocator.addAssignLikeReplicasToDifferentNodesConstraint()
+	allocator.addAdhereToNodeDiskSpaceConstraint()
+	status, allocation := allocator.allocate()
+	expectedAllocation := Assignments{
+		0: {2},
+		1: {1},
+		2: {0},
+		3: {0},
+		4: {0},
+		5: {0},
+		6: {0},
+		7: {0},
+		8: {0},
+		9: {0},
+	}
+	require.True(t, status)
+	require.Equal(t, expectedAllocation, allocation)
+}
+
+func TestGIVENCapacityConstraintAndReplicationConstraintWHENMultipleAllocationsPossibleTHENAllocationSucceeds(t *testing.T) {
+	const RangeSizeLimit = 5
+	const ReplicationFactor = 3
+	const TestClusterSize = ResourceAmount(3)
+	clusterCapacities := []ResourceAmount{70, 80, 90}
+	rangeDiskSpaceDemands := []ResourceAmount{25, 10, 12, 11, 10}
+	testConfig := initConfiguration(buildCluster(TestClusterSize, clusterCapacities), ReplicationFactor)
+	rangesToAllocate := make([]_range, RangeSizeLimit)
+	for initIndex := range rangesToAllocate {
+		rangesToAllocate[initIndex] = _range{
+			rangeId: RangeId(initIndex),
+			tags:    nil,
+			demands: Demands{SizeOnDiskDemand: rangeDiskSpaceDemands[initIndex]},
+		}
+	}
+	allocator := initAllocator(rangesToAllocate, testConfig)
+	allocator.addAssignLikeReplicasToDifferentNodesConstraint()
+	allocator.addAdhereToNodeDiskSpaceConstraint()
+	status, allocation := allocator.allocate()
+	require.True(t, status)
+	for _, nodeAssignments := range allocation {
+		require.Equal(t, len(nodeAssignments), ReplicationFactor)
+		require.True(t, isValidNodeAssignment(nodeAssignments, TestClusterSize))
+		require.True(t, isEachReplicaAssignedToDifferentNode(nodeAssignments, TestClusterSize))
+	}
+	require.True(t, nodeCapacityIsRespected(allocation, clusterCapacities, rangeDiskSpaceDemands))
+}
+
+func TestGIVENCapacityConstraintAndReplicationConstraintWHENInsufficientResourcesTHENAllocationFails(t *testing.T) {
+	const RangeSizeLimit = 5
+	const ReplicationFactor = 5
+	const TestClusterSize = ResourceAmount(3)
+	clusterCapacities := []ResourceAmount{70, 80, 90}
+	rangeDiskSpaceDemands := []ResourceAmount{25, 10, 12, 11, 10}
+	testConfig := initConfiguration(buildCluster(TestClusterSize, clusterCapacities), ReplicationFactor)
+	rangesToAllocate := make([]_range, RangeSizeLimit)
+	for initIndex := range rangesToAllocate {
+		rangesToAllocate[initIndex] = _range{
+			rangeId: RangeId(initIndex),
+			tags:    nil,
+			demands: Demands{SizeOnDiskDemand: rangeDiskSpaceDemands[initIndex]},
+		}
+	}
+	allocator := initAllocator(rangesToAllocate, testConfig)
+	allocator.addAssignLikeReplicasToDifferentNodesConstraint()
+	allocator.addAdhereToNodeDiskSpaceConstraint()
+	status, allocation := allocator.allocate()
+	require.False(t, status)
+	require.Nil(t, allocation)
+}
+
+func TestGIVENCapacityConstraintWHENInsufficientNodeCapacityTHENAllocationFails(t *testing.T) {
+	const RangeSizeLimit = 10
+	const ReplicationFactor = 1
+	const TestClusterSize = ResourceAmount(3)
+	testConfig := initConfiguration(buildCluster(TestClusterSize, []ResourceAmount{70, 80, 80}), ReplicationFactor)
+	rangesToAllocate := make([]_range, RangeSizeLimit)
+	rangeDiskSpaceDemands := [RangeSizeLimit]ResourceAmount{85, 75, 12, 11, 10, 9, 8, 7, 6, 6}
+	for initIndex := range rangesToAllocate {
+		rangesToAllocate[initIndex] = _range{
+			rangeId: RangeId(initIndex),
+			tags:    nil,
+			demands: Demands{SizeOnDiskDemand: rangeDiskSpaceDemands[initIndex]},
+		}
+	}
+	allocator := initAllocator(rangesToAllocate, testConfig)
+	allocator.addAssignLikeReplicasToDifferentNodesConstraint()
+	allocator.addAdhereToNodeDiskSpaceConstraint()
+	status, allocation := allocator.allocate()
+	require.False(t, status)
+	require.Nil(t, allocation)
+}
+
+func buildCluster(clusterSize ResourceAmount, nodeCapacities []ResourceAmount) Cluster {
+	cluster := make(Cluster, clusterSize)
+	for index := 0; index < len(cluster); index++ {
+		cluster[index] = node{
+			nodeId:    NodeId(index),
+			tags:      Tags{},
+			resources: Resources{DiskCapacityResource: nodeCapacities[index]},
+		}
+	}
+	return cluster
+}
+
+func nodeCapacitySupplier(clusterSize ResourceAmount, minCapacity ResourceAmount, maxCapacity ResourceAmount) []ResourceAmount {
+	nodeCapacities := make([]ResourceAmount, clusterSize)
+	for index := 0; index < len(nodeCapacities); index++ {
+		nodeCapacities[index] = generateRandomIntInRange(minCapacity, maxCapacity)
+	}
+	return nodeCapacities
+}
+
+func generateRandomIntInRange(lower ResourceAmount, upper ResourceAmount) ResourceAmount {
+	return ResourceAmount(rand.Int63n(int64(upper-lower)) + int64(lower))
+}
+
+func isValidNodeAssignment(nodeIds []NodeId, clusterSize ResourceAmount) bool {
+	for index := 0; index < len(nodeIds); index++ {
+		if nodeIds[index] < 0 || ResourceAmount(nodeIds[index]) > clusterSize {
+			return false
+		}
+	}
+	return true
+}
+
+func isEachReplicaAssignedToDifferentNode(nodeIds []NodeId, clusterSize ResourceAmount) bool {
+	bitMap := make([]ResourceAmount, clusterSize)
+	for index := 0; index < len(nodeIds); index++ {
+		if bitMap[nodeIds[index]] == 1 {
+			return false
+		} else {
+			bitMap[nodeIds[index]]++
+		}
+	}
+	return true
+}
+
+func nodeCapacityIsRespected(allocation Assignments, nodeCapacities []ResourceAmount, rangeDemands []ResourceAmount) bool {
+	verifierMap := make(map[NodeId]ResourceAmount)
+	for _range, nodeAssignments := range allocation {
+		for _, node := range nodeAssignments {
+			verifierMap[node] += rangeDemands[_range]
+		}
+	}
+
+	for nodeId, nodeCapacityConsumed := range verifierMap {
+		if nodeCapacityConsumed > nodeCapacities[nodeId] {
+			return false
+		}
+	}
+	return true
 }
