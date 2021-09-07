fb917c3eb1a088a2983c735050fe1f45c731e29b
Saad Cheema <saad_mukarram@hotmail.com>
2021-09-07 14:21:37 -0400

    Revert renaming of Allocator struct, general re-naming.

--- allocator.go
+++ allocator.go
@@ -31,9 +31,9 @@ type options struct {
 	withTagAffinity  bool
 }
 
-type OptionConfigurator func(*options)
+type Option func(*options)
 
-type allocator struct {
+type Allocator struct {
 	ranges     []Range
 	nodes      []Node
 	model      *cpsatsolver.Model
@@ -58,7 +58,7 @@ func NewNode(id NodeID, tags []string, resources map[Resource]int64) Node {
 	}
 }
 
-func New(ranges []Range, nodes []Node, opts ...OptionConfigurator) *allocator {
+func New(ranges []Range, nodes []Node, opts ...Option) *Allocator {
 	model := cpsatsolver.NewModel()
 	assignment := make(map[RangeID]map[NodeID]cpsatsolver.Literal)
 	for _, r := range ranges {
@@ -67,12 +67,12 @@ func New(ranges []Range, nodes []Node, opts ...OptionConfigurator) *allocator {
 			assignment[r.id][n.id] = model.NewLiteral(fmt.Sprintf("r%d-on-n%d", r.id, n.id))
 		}
 	}
-	defaultOptions := options{withTagAffinity: false, withNodeCapacity: false}
+	defaultOptions := options{}
 	for _, opt := range opts {
 		opt(&defaultOptions)
 	}
 
-	return &allocator{
+	return &Allocator{
 		ranges:     ranges,
 		nodes:      nodes,
 		model:      model,
@@ -81,7 +81,7 @@ func New(ranges []Range, nodes []Node, opts ...OptionConfigurator) *allocator {
 	}
 }
 
-func (a *allocator) rangeLiterals(r Range) []cpsatsolver.Literal {
+func (a *Allocator) rangeLiterals(r Range) []cpsatsolver.Literal {
 	var res []cpsatsolver.Literal
 	ns := a.assignment[r.id]
 	for _, k := range ns {
@@ -90,23 +90,23 @@ func (a *allocator) rangeLiterals(r Range) []cpsatsolver.Literal {
 	return res
 }
 
-func (a *allocator) literal(r Range, n Node) cpsatsolver.Literal {
+func (a *Allocator) literal(r Range, n Node) cpsatsolver.Literal {
 	return a.assignment[r.id][n.id]
 }
 
-func WithNodeCapacity() OptionConfigurator {
+func WithNodeCapacity() Option {
 	return func(opt *options) {
 		opt.withNodeCapacity = true
 	}
 }
 
-func WithTagMatching() OptionConfigurator {
+func WithTagMatching() Option {
 	return func(opt *options) {
 		opt.withTagAffinity = true
 	}
 }
 
-func (a *allocator) adhereToNodeResources() {
+func (a *Allocator) adhereToNodeResources() {
 	for _, re := range []Resource{DiskResource} {
 		for _, n := range a.nodes {
 			capacity := n.resources[re]
@@ -125,16 +125,16 @@ func (a *allocator) adhereToNodeResources() {
 	}
 }
 
-func (a *allocator) adhereToNodeTags() {
+func (a *Allocator) adhereToNodeTags() {
 	for _, r := range a.ranges {
-		nodeThatAreNonassignable := make([]cpsatsolver.Literal, 0, len(a.nodes))
+		unAssignableNodes := make([]cpsatsolver.Literal, 0, len(a.nodes))
 		for _, n := range a.nodes {
 			if !rangeTagsAreSubsetOfNodeTags(r.tags, n.tags) {
-				nodeThatAreNonassignable = append(nodeThatAreNonassignable, a.literal(r, n))
+				unAssignableNodes = append(unAssignableNodes, a.literal(r, n))
 			}
 		}
 		a.model.AddConstraints(
-			cpsatsolver.NewExactlyKConstraint(0, nodeThatAreNonassignable...),
+			cpsatsolver.NewExactlyKConstraint(0, unAssignableNodes...),
 		)
 	}
 }
@@ -155,7 +155,7 @@ func rangeTagsAreSubsetOfNodeTags(rangeTags []string, nodeTags []string) bool {
 	return true
 }
 
-func (a *allocator) Allocate() (ok bool, assignments map[RangeID][]NodeID) {
+func (a *Allocator) Allocate() (ok bool, assignments map[RangeID][]NodeID) {
 	if a.opts.withNodeCapacity {
 		a.adhereToNodeResources()
 	}
