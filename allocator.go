c061c8768c1719fa93d412631e8b89c2305004ac
AlirezaDX <44884642+AlirezaDX@users.noreply.github.com>
2021-11-11 18:40:46 -0500

    Interface-work (#17)
    * Adding range and Node Interfaces
    
    Adding range and new interface
    
    * Adding more Node and Range Interfaces
    
    * Further Improvements to Range and Node Interface
    
    * Changed nmap and rmap to pointers
    
    * Random edits.
    
    * Update go.sum
    
    * Remove pointers from the maps
    
    * Break objects to seperate files
    Streamline interface usage with options pattern
    Modify interfaces to avoid misuse of the the allocator
    
    * Include the fix to the composition bug
    
    * Go fmt fix
    
    * Address comments
    
    Co-authored-by: musanimu <msaad.musani@mail.utoronto.ca>
    Co-authored-by: Saad Cheema <saad_mukarram@hotmail.com>
    Co-authored-by: musanimu <55329065+musanimu@users.noreply.github.com>

--- allocator.go
+++ allocator.go
@@ -8,51 +8,18 @@ import (
 	"time"
 )
 
-// nodeId is a 64-bit identifier for nodes.
-type nodeId int64
-
-// rangeId is a 64-bit identifier for ranges.
-type rangeId int64
-
-// resource is 32-bit identifier for resources -> {disk, qps}
-type resource int
-
 // Allocation is the return type of our allocator.
-// It models mappings of RangeIDs to a list of NodeIDs.
+// It models mappings of ReplicaIDs to a list of NodeIDs.
 type Allocation map[int64][]int64
 
 const (
-	noMaxChurn              = -1
-	defaultTimeout          = time.Second * 10
-	loggingPrefix           = ""
-	diskResource   resource = iota
-	qps
+	noMaxChurn     = -1
+	defaultTimeout = time.Second * 10
+	loggingPrefix  = ""
 )
 
-// ckRange encapsulates pertaining metadata for cockroachDB ranges.
-type ckRange struct {
-	// id represents a unique identifier.
-	id rangeId
-	// rf equals the replication factor of said range.
-	rf int
-	// tags are strings that showcase affinity for nodes.
-	tags []string
-	// demands model the resource requirements of said range.
-	demands map[resource]int64
-}
-
-// node encapsulates pertaining metadata for cockroachDB nodes.
-type node struct {
-	// id represents a unique identifier.
-	id nodeId
-	// tags are strings that showcase affinity for ranges.
-	tags []string
-	// resources model the resource profile of said node.
-	resources map[resource]int64
-}
-
-// options hold runtime allocation configurations.
-type options struct {
+// allocOptions hold runtime allocation configurations.
+type allocOptions struct {
 	// withResources signals the allocator to perform balancing and capacity checking.
 	withResources bool
 	// withTagAffinity forces the allocator to perform affine allocations only.
@@ -61,198 +28,79 @@ type options struct {
 	withMinimalChurn bool
 	// maxChurn limits the number of moves needed to fulfill an allocation request with respect to a prior allocation.
 	maxChurn int64
-	// prevAssignment holds some prior allocation.
-	prevAssignment map[rangeId][]nodeId
 	// searchTimeout forces the solver to return within the specified duration.
 	searchTimeout time.Duration
 	// verboseLogging routes all the internal solver logs to stdout.
 	verboseLogging bool
 }
 
-// Option manifests a closure that mutates allocation configurations in accordance with caller preferences.
-type Option func(*options)
+// AllocOption manifests a closure that mutates allocation configurations in accordance with caller preferences.
+type AllocOption func(*allocOptions)
 
-// Allocator holds the ranges, nodes, underlying CP-SAT solver, assigment variables, and configuration needed.
-type Allocator struct {
-	// ranges are a mapping of rangeId onto the ckRange struct.
-	ranges map[rangeId]*ckRange
-	// nodes are a mapping of nodeId onto the node struct.
-	nodes map[nodeId]*node
+// allocator holds the replicas, nodes, underlying CP-SAT solver, assigment variables, and configuration needed.
+type allocator struct {
+	*ClusterState
 	// model is the underlying CP-SAT solver and the engine of this package.
 	model *solver.Model
 	// assignment represents variables that we constrain and impose on to satisfy allocation requirements.
-	assignment map[rangeId][]solver.IntVar
+	assignment map[replicaId][]solver.IntVar
 	// opts hold allocation configurations -> {withResources, withTagAffinity...}
-	opts options
+	opts allocOptions
 }
 
-type Cluster struct {
-	nodes map[nodeId]*node
-}
-type Items struct {
-	ranges map[rangeId]*ckRange
-}
-
-func NewCluster() Cluster {
-	return Cluster{
-		nodes: make(map[nodeId]*node),
+// newAllocator builds, configures, and returns an allocator from the necessary parameters.
+// Note this allocator should not be reused after solving a problem because the underlying solver is stateful
+func newAllocator(cs *ClusterState, opts ...AllocOption) *allocator {
+	model := solver.NewModel("Lé-allocator")
+	assignment := make(map[replicaId][]solver.IntVar)
+	defaultOptions := allocOptions{
+		// assume no maxChurn initially, let the allocOptions slice override if needed.
+		maxChurn:      noMaxChurn,
+		searchTimeout: defaultTimeout,
 	}
-}
 
-func NewItems() Items {
-	return Items{
-		ranges: make(map[rangeId]*ckRange),
-	}
-}
-
-// AddNode adds a node into the cluster collection.
-func (c *Cluster) AddNode(id int64, nodeCapacity int64, tags ...string) {
-	c.nodes[nodeId(id)] = &node{
-		id:        nodeId(id),
-		tags:      tags,
-		resources: map[resource]int64{diskResource: nodeCapacity},
-	}
-}
-
-// UpdateNodeTags updates the tags for a node currently residing in the cluster collection,
-// returning true for a successful update, false if the nodeID does not map to any node.
-func (c Cluster) UpdateNodeTags(id int64, tags ...string) bool {
-	if n, found := c.nodes[nodeId(id)]; found {
-		n.tags = tags
-		return true
-	}
-	return false
-}
-
-// UpdateNodeCapacity updates the disk-capacity for a node currently residing in the cluster collection,
-// returning true for a successful update, false if the nodeID does not map to any node.
-func (c Cluster) UpdateNodeCapacity(id int64, nodeCapacity int64) bool {
-	if n, found := c.nodes[nodeId(id)]; found {
-		n.resources[diskResource] = nodeCapacity
-		return true
-	}
-	return false
-}
-
-// RemoveNode removes a given node from the cluster collection, returning true is the deletion is successful,
-// false if the nodeID does not map to any node.
-func (c *Cluster) RemoveNode(id int64) bool {
-	if _, found := c.nodes[nodeId(id)]; found {
-		delete(c.nodes, nodeId(id))
-		return true
-	}
-	return false
-}
-
-// AddRange adds a range into the items' collection.
-func (i *Items) AddRange(id int64, rf int, diskDemand int64, q int64, tags ...string) {
-	i.ranges[rangeId(id)] = &ckRange{
-		id:      rangeId(id),
-		rf:      rf,
-		tags:    tags,
-		demands: map[resource]int64{diskResource: diskDemand, qps: q},
-	}
-}
-
-// RemoveRange removes a given range from the items' collection, returning true is the deletion is successful,
-// false if the rangeID does not map to any range.
-func (i *Items) RemoveRange(id int64) bool {
-	if _, found := i.ranges[rangeId(id)]; found {
-		delete(i.ranges, rangeId(id))
-		return true
-	}
-	return false
-}
-
-// UpdateRangeTags updates the tags for a range currently residing in the items' collection,
-// returning true for a successful update, false if the rangeID does not map to any range.
-func (i *Items) UpdateRangeTags(id int64, tags ...string) bool {
-	if r, found := i.ranges[rangeId(id)]; found {
-		r.tags = tags
-		return true
-	}
-	return false
-}
-
-// UpdateRangeDiskDemand updates the disk-demand for a range currently residing in the items' collection,
-// returning true for a successful update, false if the rangeID does not map to any range.
-func (i *Items) UpdateRangeDiskDemand(id rangeId, diskDemand int64) bool {
-	if r, found := i.ranges[id]; found {
-		r.demands[diskResource] = diskDemand
-		return true
-	}
-	return false
-}
-
-// UpdateRangeQps updates the qps for a range currently residing in the items' collection,
-// returning true for a successful update, false if the rangeID does not map to any range.
-func (i *Items) UpdateRangeQps(id int64, q int64) bool {
-	if r, found := i.ranges[rangeId(id)]; found {
-		r.demands[qps] = q
-		return true
-	}
-	return false
-}
-
-// New builds, configures, and returns an allocator from the necessary parameters.
-func New(opts ...Option) *Allocator {
-	model := solver.NewModel("Lé-Allocator")
-	assignment := make(map[rangeId][]solver.IntVar)
-	defaultOptions := options{}
-	// assume no maxChurn initially, let the options slice override if needed.
-	defaultOptions.maxChurn = noMaxChurn
-	defaultOptions.searchTimeout = defaultTimeout
 	for _, opt := range opts {
 		opt(&defaultOptions)
 	}
 
-	return &Allocator{
-		model:      model,
-		assignment: assignment,
-		opts:       defaultOptions,
+	return &allocator{
+		ClusterState: cs,
+		model:        model,
+		assignment:   assignment,
+		opts:         defaultOptions,
 	}
+}
+
+func Solve(cs *ClusterState, opts ...AllocOption) (ok bool, allocation Allocation) {
+	return newAllocator(cs, opts...).allocate()
 }
 
 // Print is a utility method that pretty-prints allocation information.
 func (a Allocation) Print() {
-	for rangeID, nodeIDs := range a {
-		fmt.Println("ckRange with ID: ", rangeID, " on nodes: ", nodeIDs)
+	for replicaID, nodeIDs := range a {
+		fmt.Println("replica with ID: ", replicaID, " on nodes: ", nodeIDs)
 	}
 }
 
 // WithResources is a closure that configures the allocator to adhere to capacity constraints and load-balance across
 // resources.
-func WithResources() Option {
-	return func(opt *options) {
+func WithResources() AllocOption {
+	return func(opt *allocOptions) {
 		opt.withResources = true
 	}
 }
 
 // WithTagMatching is a closure that configures the allocator to perform affine allocations only.
-func WithTagMatching() Option {
-	return func(opt *options) {
+func WithTagMatching() AllocOption {
+	return func(opt *allocOptions) {
 		opt.withTagAffinity = true
-	}
-}
-
-// WithPriorAssignment is a closure that ingests a prior allocation.
-func WithPriorAssignment(prevAssignment map[int64][]int64) Option {
-	temp := make(map[rangeId][]nodeId)
-	for r, n := range prevAssignment {
-		temp[rangeId(r)] = make([]nodeId, len(n))
-		for i, nid := range n {
-			temp[rangeId(r)][i] = nodeId(nid)
-		}
-	}
-	return func(opt *options) {
-		opt.prevAssignment = temp
 	}
 }
 
 // WithMaxChurn is a closure that inspects and sets a hard limit on the maximum number of moves deviating
 // from some prior assignment.
-func WithMaxChurn(maxChurn int64) Option {
-	return func(opt *options) {
+func WithMaxChurn(maxChurn int64) AllocOption {
+	return func(opt *allocOptions) {
 		if maxChurn < 0 {
 			panic("max-churn must be greater than or equal to 0")
 		}
@@ -261,59 +109,59 @@ func WithMaxChurn(maxChurn int64) Option {
 }
 
 // WithChurnMinimized is a closure that configures the allocator to minimize variance from some prior allocation.
-func WithChurnMinimized() Option {
-	return func(opt *options) {
+func WithChurnMinimized() AllocOption {
+	return func(opt *allocOptions) {
 		opt.withMinimalChurn = true
 	}
 }
 
 // WithTimeout is a closure that configures the allocator to conclude its search within the duration provided.
-func WithTimeout(searchTimeout time.Duration) Option {
-	return func(opt *options) {
+func WithTimeout(searchTimeout time.Duration) AllocOption {
+	return func(opt *allocOptions) {
 		opt.searchTimeout = searchTimeout
 	}
 }
 
 // WithVerboseLogging is a closure that forces our solver to expose its logs to the caller for inspection.
-func WithVerboseLogging() Option {
-	return func(opt *options) {
+func WithVerboseLogging() AllocOption {
+	return func(opt *allocOptions) {
 		opt.verboseLogging = true
 	}
 }
 
-func (a *Allocator) adhereToResourcesAndBalance() {
+func (a *allocator) adhereToResourcesAndBalance() {
 	// build a fixed offset of size one initially to avoid polluting the constant set with unnecessary variables.
 	// we can use this across loop iterations, since this is used only to indicate the distance between intervals starts + ends.
 	fixedSizedOneOffset := a.model.NewConstant(1, fmt.Sprintf("Fixed offset of size 1."))
-	for _, re := range []resource{diskResource, qps} {
+	for _, re := range []Resource{DiskResource, QPS} {
 		rawCapacity := int64(0)
 		// compute availability of node capacity. If not defined, assume we have just enough to
 		// allocate the entire load on EACH node. This helps keep our bounds tight, as opposed to an arbitrary number.
 		if c, ok := a.nodes[0].resources[re]; ok {
 			rawCapacity = c
 		} else {
-			for _, r := range a.ranges {
+			for _, r := range a.replicas {
 				rawCapacity += r.demands[re]
 			}
 		}
-		capacity := a.model.NewIntVar(0, rawCapacity, fmt.Sprintf("IV used to minimize variance and enforce capacity constraint for resource: %d", re))
+		capacity := a.model.NewIntVar(0, rawCapacity, fmt.Sprintf("IV used to minimize variance and enforce capacity constraint for Resource: %d", re))
 		tasks := make([]solver.Interval, 0)
-		// demands represent the resource requirements placed on each node by potential matches to a range.
+		// demands represent the resource requirements placed on each node by potential matches to a replica.
 		demands := make([]solver.IntVar, 0)
 		for rID, nIDs := range a.assignment {
 			for i, id := range nIDs {
-				// go over rangeIDs and their respective ivs.
-				// for that specific range, tell the allocator "regardless of where you place this replica, you will
+				// go over replicaIDs and their respective ivs.
+				// for that specific replica, tell the allocator "regardless of where you place this replica, you will
 				// pay a cost of r.resource[re]". What we're asking the allocator to do is then arrange the intervals
 				// in a fashion that does not violate our capacity requirements.
 				toAdd := a.model.NewInterval(
 					id,
 					a.model.NewIntVarFromDomain(solver.NewDomain(1, int64(len(a.nodes))), "Adjusted intervals for upper bounds."),
 					fixedSizedOneOffset,
-					fmt.Sprintf("Interval representing demands placed on node by range: %d, replica: %d", rID, i),
+					fmt.Sprintf("Interval representing demands placed on node by replica: %d, replica: %d", rID, i),
 				)
 				tasks = append(tasks, toAdd)
-				demands = append(demands, a.model.NewConstant(a.ranges[rID].demands[re], fmt.Sprintf("Demand for r.id:%d.", rID)))
+				demands = append(demands, a.model.NewConstant(a.replicas[rID].demands[re], fmt.Sprintf("Demand for r.id:%d.", rID)))
 			}
 		}
 		// set ceiling for interval interleaving.
@@ -326,12 +174,12 @@ func (a *Allocator) adhereToResourcesAndBalance() {
 	}
 }
 
-func (a *Allocator) adhereToNodeTags() {
-	for rID, r := range a.ranges {
+func (a *allocator) adhereToNodeTags() {
+	for rID, r := range a.replicas {
 		forbiddenAssignments := make([][]int64, 0)
-		// for each range-node pair, if incompatible, force the allocator to write-off said allocation.
+		// for each replica-node pair, if incompatible, force the allocator to write-off said allocation.
 		for nID, n := range a.nodes {
-			if !rangeTagsAreSubsetOfNodeTags(r.tags, n.tags) {
+			if !replicaTagsAreSubsetOfNodeTags(r.tags, n.tags) {
 				forbiddenAssignments = append(forbiddenAssignments, []int64{int64(nID)})
 			}
 		}
@@ -343,12 +191,12 @@ func (a *Allocator) adhereToNodeTags() {
 	}
 }
 
-// rangeTagsAreSubsetOfNodeTags returns true iff a range's tags are a subset of a node's tags
-func rangeTagsAreSubsetOfNodeTags(rangeTags []string, nodeTags []string) bool {
-	for _, rangeTag := range rangeTags {
+// replicaTagsAreSubsetOfNodeTags returns true iff a replica's tags are a subset of a node's tags
+func replicaTagsAreSubsetOfNodeTags(replicaTags []string, nodeTags []string) bool {
+	for _, replicaTag := range replicaTags {
 		foundMatch := false
 		for _, nodeTag := range nodeTags {
-			if rangeTag == nodeTag {
+			if replicaTag == nodeTag {
 				foundMatch = true
 				break
 			}
@@ -360,23 +208,23 @@ func rangeTagsAreSubsetOfNodeTags(rangeTags []string, nodeTags []string) bool {
 	return true
 }
 
-func (a *Allocator) adhereToChurnConstraint() {
-	if a.opts.prevAssignment == nil {
+func (a *allocator) adhereToChurnConstraint() {
+	if a.currentAssignment == nil {
 		panic("missing/invalid prior assignment")
 	}
 	toMinimizeTheSumLiterals := make([]solver.Literal, 0)
 	fixedDomain := solver.NewDomain(0, 0)
 
-	for _, r := range a.ranges {
-		// go over ranges, if a range was previously assigned to some node, attempt to keep that assignment as long as
+	for _, r := range a.replicas {
+		// go over replicas, if a replica was previously assigned to some node, attempt to keep that assignment as long as
 		// said node still exists in the cluster.
-		if prevNodeIDs, ok := a.opts.prevAssignment[r.id]; ok {
+		if prevNodeIDs, ok := a.currentAssignment[r.id]; ok {
 			for i, iv := range a.assignment[r.id] {
 				if _, ok := a.nodes[prevNodeIDs[i]]; ok {
-					newLiteral := a.model.NewLiteral(fmt.Sprintf("Literal tracking variance between assignment of range:%d, replica:%d on node:%d", r.id, i, prevNodeIDs[i]))
+					newLiteral := a.model.NewLiteral(fmt.Sprintf("Literal tracking variance between assignment of replica:%d, replica:%d on node:%d", r.id, i, prevNodeIDs[i]))
 					a.model.AddConstraints(
 						solver.NewLinearConstraint(
-							solver.NewLinearExpr([]solver.IntVar{iv, a.model.NewConstant(int64(prevNodeIDs[i]), fmt.Sprintf("IntVar corresponding to assignment of range:%d, replica:%d on node:%d", r.id, i, prevNodeIDs[i]))},
+							solver.NewLinearExpr([]solver.IntVar{iv, a.model.NewConstant(int64(prevNodeIDs[i]), fmt.Sprintf("IntVar corresponding to assignment of replica:%d, replica:%d on node:%d", r.id, i, prevNodeIDs[i]))},
 								[]int64{1, -1}, 0), fixedDomain).OnlyEnforceIf(newLiteral))
 					toMinimizeTheSumLiterals = append(toMinimizeTheSumLiterals, newLiteral.Not())
 				}
@@ -397,14 +245,13 @@ func (a *Allocator) adhereToChurnConstraint() {
 	}
 }
 
-// Allocate is a terminal method call that returns a status and paired allocation.
+// allocate is a terminal method call that returns a status and paired allocation.
 // The status could be false if the existing model is invalid or unsatisfiable.
-func (a *Allocator) Allocate(cluster Cluster, items Items) (ok bool, allocation Allocation) {
-	a.nodes = cluster.nodes
-	a.ranges = items.ranges
-	// iterate over ranges, assign each rangeID a list of IV's sized r.rf.
-	// These will ultimately then read as: rangeID's replicas assigned to nodes [N.1, N.2,...N.RF]
-	for _, r := range a.ranges {
+func (a *allocator) allocate() (ok bool, allocation Allocation) {
+
+	// iterate over replicas, assign each replicaID a list of IV's sized r.rf.
+	// These will ultimately then read as: replicaID's replicas assigned to nodes [N.1, N.2,...N.RF]
+	for _, r := range a.replicas {
 		a.assignment[r.id] = make([]solver.IntVar, r.rf)
 		for j := range a.assignment[r.id] {
 			// constrain our IV's to live between [0, len(nodes) - 1].
@@ -449,7 +296,7 @@ func (a *Allocator) Allocate(cluster Cluster, items Items) (ok bool, allocation 
 	}
 
 	res := make(Allocation)
-	for rID, r := range a.ranges {
+	for rID, r := range a.replicas {
 		nodes := a.assignment[rID]
 		for _, n := range nodes {
 			allocated := result.Value(n)
